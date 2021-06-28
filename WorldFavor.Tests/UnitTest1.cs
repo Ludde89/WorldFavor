b4804bf990e1229fc8cf24c8738fc47b70335df0
Ludwig <ludwig.hedberg89@gmail.com>
2021-06-28 17:58:18 +0200

    structure for unit test

--- WorldFavor.Tests/UnitTest1.cs
+++ WorldFavor.Tests/UnitTest1.cs
@@ -1,13 +1,63 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WorldFavor.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 : TestBase
     {
+        private HttpResponseMessage _actual;
+
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(_actual.StatusCode, HttpStatusCode.OK);
         }
+
+        protected override async Task Act()
+        {
+            var client = TestServer.GetTestClient();
+
+            _actual = await client.GetAsync("/api/books/foo");
+        }
+    }
+
+    public abstract class TestBase
+    {
+        protected IHost TestServer;
+
+        [TestInitialize]
+        public async Task Run()
+        {
+            SetUpDependencies();
+            await Act();
+        }
+
+        private void SetUpDependencies()
+        {
+            var hostBuilder = new HostBuilder();
+            hostBuilder
+                .ConfigureWebHost(webHost =>
+                {
+                    webHost.UseTestServer()
+                        .UseStartup<Startup>()
+                        .ConfigureServices(SetUpDependencies);
+                });
+
+            TestServer = hostBuilder.Build();
+            TestServer.Start();
+        }
+
+        protected virtual void SetUpDependencies(IServiceCollection services) { }
+
+        protected abstract Task Act();
     }
 }
---
+++ WorldFavor/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldFavor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{title}")]
+        public async Task<ActionResult<Book>> Get(string title)
+        {
+            return Ok( new Book(title));
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+    }
+}
